4d70648c8b06c08deb2ea66d4dffce033374b407
Vivek Patil <121660547+vivekpatil1995@users.noreply.github.com>
2023-11-02 16:34:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,6 @@ streamlit.text('🎂Omega 3 & Blueberry Oatmeal')
 streamlit.text('☕Kale,Spinach & Rocket Smoothie')
 streamlit.text('🎁Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍆Avocado Toast🥦🥬')
+import pandas 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dadw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)

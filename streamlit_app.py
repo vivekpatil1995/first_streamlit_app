fae4e9d4a0a2bb3e21b1f4d663d08683dd1bde9a
Vivek Patil <121660547+vivekpatil1995@users.noreply.github.com>
2023-11-02 17:10:21 +0530

    Update streamlit_app.py
    Multiselect statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,5 @@ streamlit.text('🎁Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍆Avocado Toast🥦🥬')
 import pandas 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.multiselect("pick some fruits:",list(my_fruit_list))
 streamlit.dataframe(my_fruit_list)

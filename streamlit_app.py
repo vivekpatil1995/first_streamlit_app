8fa8b486c30fb4c84bd8e8f9e2777e5a050354b2
Vivek Patil <121660547+vivekpatil1995@users.noreply.github.com>
2023-11-02 16:21:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('🎂Omega 3 & Blueberry Oatmeal')
+streamlit.text(☕'Kale,Spinach & Rocket Smoothie')
+streamlit.text('🎁Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍆Avocado Toast🥦🥬')
+

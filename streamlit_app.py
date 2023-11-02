531a8803cd09608be3c239ebcf0042fd36abdca8
Vivek Patil <121660547+vivekpatil1995@users.noreply.github.com>
2023-11-03 01:10:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,5 +16,5 @@ streamlit.dataframe(fruits_to_show)
 #new session
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 streamlit.header("Fruityvice Fruit Advice!")
